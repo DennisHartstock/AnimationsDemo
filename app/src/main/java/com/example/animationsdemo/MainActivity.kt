80cc42ed7079f503755d93744b82350846c8f302
DennsHartstock <dennis.hartstock@web.de>
2022-03-12 07:33:09 +0100

    implemented gradient animation

--- app/src/main/java/com/example/animationsdemo/MainActivity.kt
+++ app/src/main/java/com/example/animationsdemo/MainActivity.kt
@@ -4,17 +4,24 @@ import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var animationImageView = findViewById<ImageView>(R.id.animationImageview)
+        val animationImageView = findViewById<ImageView>(R.id.animationImageview)
+        val gradientOverlay = findViewById<LinearLayout>(R.id.gradientOverlay)
 
-        var background = animationImageView.background as AnimationDrawable
+        val background = animationImageView.background as AnimationDrawable
         background.setEnterFadeDuration(3000)
         background.setExitFadeDuration(1000)
         background.start()
+
+        val overlay = gradientOverlay.background as AnimationDrawable
+        overlay.setEnterFadeDuration(3000)
+        overlay.setExitFadeDuration(1000)
+        overlay.start()
     }
 }
